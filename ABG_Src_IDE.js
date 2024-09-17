14cb3af2618e32ac1b9c63b5eb8dbfd0f7de0233
Christian Kowalik <Christiank1996@hotmail.de>
2024-09-17 20:42:13 +0200

    Übung ABG-03: Web-Server

--- ABG_Src_IDE.js
+++
@@ -1,6 +0,0 @@
-function numbers() {
-    document.write("Numbers: ");
-    for (var i = 0; i < 1000; i++) {
-        document.write(i + " ");
-    }
-}
--- README.md
+++
@@ -1,2 +0,0 @@
-# EA-1
-Graphical Visualisation Technologies 2024/2025
